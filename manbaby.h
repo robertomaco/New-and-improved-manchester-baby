07ec5d826cf43910d8ccac5f4d3427e198cd14b3
maccamcmacca <maccamcmacca@users.noreply.github.com>
2016-11-04 12:31:22 +0000

    Update manbaby.h

--- manbaby.h
+++ manbaby.h
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+
 
 using namespace std;
 
@@ -36,5 +40,37 @@ char cleararr(char l[]){
 
 }
 
+void printmem(char l[][32]){
+
+	for(int x = 0; x<32; x++){
+	for(int i = 0; i<32; i++){
+
+		cout << l[x][i];
+
+	}
+	cout << endl;
+}
+
+}
+
+char poke(char l[], char x[][32]){
+
+	int line;
+
+	cout << "Enter the line which you would like to poke data into, from 1 to 32: ";
+	cin >> line;
+	cin.ignore();
+	cout << endl << "Enter a 32 bit string: ";
+	cin.getline(l, 32);
+
+	//for(int i = 0; i<32;i++){
+		for(int p = 0; p<32; p++){
+			x[line-1][p] = l[p];
+		}
+	//}
+
+
+
+}
 
 
